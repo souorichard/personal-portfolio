9d5c3d36bb97c30c00b3187f2d8633f7da27280a
souorichard <elaine.ra96@gmail.com>
2024-08-28 11:20:28 -0300

    feat: apply components to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
 
-const inter = Inter({ subsets: ['latin'] })
+import { cn } from '@/lib/utils'
+
+const rubik = Rubik({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Portfolio | Richard Rodrigues',
@@ -17,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn('min-h-screen tracking-wide', rubik.className)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          <main className="max-w-2xl mx-auto px-8 py-20">{children}</main>
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+import { AboutSection } from '@/components/about-section'
+import { ExperiencesSession } from '@/components/experiences-session'
+import { Header } from '@/components/header'
+import { ProjectsSession } from '@/components/projects-session'
+
 export default function Home() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <Header />
+
+      <main className="space-y-12">
+        <AboutSection />
+        <ProjectsSession />
+        <ExperiencesSession />
+      </main>
+    </>
+  )
 }
